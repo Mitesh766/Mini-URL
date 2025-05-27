774d2492c15353aceaa2f1bac8a503770fd744b5
Mitesh Agrawal <miteshagrawal972@gmail.com>
2025-05-27 16:01:08 +0530

    frontend : configuration update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
@@ -14,8 +13,4 @@ export default defineConfig({
       },
     },
   },
-   build: {
-    outDir: 'build',  
-  },
 });
-
