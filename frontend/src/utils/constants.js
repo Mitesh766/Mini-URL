c8b27564b62832caa3203dba9e485e3a42906713
Mitesh Agrawal <miteshagrawal972@gmail.com>
2025-05-27 16:59:55 +0530

    updated backend url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,3 @@
-const BACKEND_URL="https://mini-url-4cbj.onrender.com"
+const BACKEND_URL="https://minli.up.railway.app"
 export const USERS_URL = BACKEND_URL+"/api/user"
 export const API_URL=BACKEND_URL+"/api/url"
