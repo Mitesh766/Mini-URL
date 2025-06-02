9a82bbd5baeb2040f44f7ffd53c51ef93e50ff3c
Mitesh Agrawal <miteshagrawal972@gmail.com>
2025-06-02 19:57:15 +0530

    backend : bug fix

--- backend/app.js
+++ backend/app.js
@@ -64,7 +64,7 @@ app.get(/^\/(?!api).*/, (req, res) => {
 
 const PORT=process.env.PORT || 3000
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0' ,() => {
   console.log("Server successfully started on port 3000");
 });
 
